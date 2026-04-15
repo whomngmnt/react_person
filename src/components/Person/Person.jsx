b91e47cf0be7a3d39ab20b36fe019c7bd189a631
Dmytro Khromiak <whomngmnt@gmail.com>
2026-04-15 18:23:02 +0300

    prvided Lukes requirments

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,19 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const partnerRole = sex === 'm' ? 'wife' : 'husband';
+
+  const partnerText = isMarried
+    ? `${partnerName} is my ${partnerRole}`
+    : 'I am not married';
+
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
 
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {age && <p className="Person__age">I am {age}</p>}
 
-      <p className="Person__partner">
-        {person.isMarried
-          ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
-          : 'I am not married'}
-      </p>
+      <p className="Person__partner">{partnerText}</p>
     </section>
   );
 };
